01d63b07fcfa798485a14b5534077db46f7ca23f
Garrett Goon <garrett.goon@hpe.com>
2023-03-11 16:13:43 -0500

    fine tuning

--- users/garrett361/config.h
+++ users/garrett361/config.h
@@ -1,4 +1,4 @@
-// Copyright 2019 Manna Harbour
+// Copyright 2020 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
 // This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
@@ -7,7 +7,7 @@
 
 // default but used in macros
 #undef TAPPING_TERM
-#define TAPPING_TERM 150
+#define TAPPING_TERM 160
 
 // Prevent normal rollover on alphas from accidentally triggering mods.
 #define IGNORE_MOD_TAP_INTERRUPT
@@ -15,6 +15,8 @@
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define TAPPING_FORCE_HOLD
 
+// Hold both shifts for caps word.
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
 
 // Mouse key speed and acceleration.
 #undef MOUSEKEY_DELAY
@@ -28,4 +30,6 @@
 #undef MOUSEKEY_TIME_TO_MAX
 #define MOUSEKEY_TIME_TO_MAX    64
 
-
+//
+#define TAP_CODE_DELAY 0
+#define TAPPING_TOGGLE 5
