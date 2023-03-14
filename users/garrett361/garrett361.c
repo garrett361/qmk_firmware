b3a2cb7892987d2303279bccc185b2f7cad03f4b
Garrett Goon <garrett.goon@hpe.com>
2023-03-14 10:43:04 -0400

    implemented constant consistent keymapping with key swaps done at specific keyboard level

--- users/garrett361/garrett361.c
+++ users/garrett361/garrett361.c
@@ -29,13 +29,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,                               KC_Y,        KC_U,         KC_I,         KC_O,            KC_P,
         LCTL_T(KC_A), LALT_T(KC_S), LCMD_T(KC_D), LSFT_T(KC_F), KC_G,                               KC_H,RSFT_T(KC_J), RCMD_T(KC_K), LALT_T(KC_L), RCTL_T(KC_QUOT),
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                                                   KC_N,        KC_M,      KC_COMM,       KC_DOT,         KC_SLSH,
-        LT(NUM, KC_BSPC), LT(NAV, KC_ENT), LT(MOUSE, KC_ESC),                                                        LT(SYM_2, KC_DEL),  LT(SYM_1, KC_TAB), KC_SPC
+        LT(NAV, KC_ENT), LT(NUM, KC_BSPC), LT(MOUSE, KC_ESC),                                                        LT(SYM_2, KC_DEL),  KC_SPC, LT(SYM_1, KC_TAB)
     ),
     [NUM] = LAYOUT_garrett361(
         _______,    _______,    _______,    _______,    _______,                               _______,    KC_7,       KC_8,       KC_9,    _______,
         _______,    _______,    _______,    _______,    _______,                               _______,    KC_4,       KC_5,       KC_6,    _______,
         _______,    _______,    _______,    _______,    _______,                               _______,    KC_1,       KC_2,       KC_3,    _______,
-                                _______,    _______,    _______,                               _______,    _______,    KC_0
+                                _______,    _______,    _______,                               _______,    KC_0,       _______
     ),
     [NAV] = LAYOUT_garrett361(
         _______,    _______,    _______,    _______,    _______,                               KC_HOME,    KC_PGDN,    KC_PGUP,    KC_END,     _______,
