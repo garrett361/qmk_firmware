6c0a8f82054fd4606715a2c8f01921a01ecd1437
Garrett Goon <garrett.goon@hpe.com>
2023-08-24 22:52:32 -0400

    clang format protection

--- layouts/community/split_3x6_3/garrett361/config.h
+++ layouts/community/split_3x6_3/garrett361/config.h
@@ -3,6 +3,7 @@
 
 // This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+// clang-format off
 #pragma once
 
 #define XXX KC_NO
@@ -19,3 +20,4 @@ XXX,  K10,  K11,  K12,  K13,  K14,         K15,  K16,  K17,  K18,  K19,  XXX, \
 XXX,  K20,  K21,  K22,  K23,  K24,         K25,  K26,  K27,  K28,  K29,  XXX , \
                   K32,  K33,  K34,         K35,  K36,  K37 \
 )
+// clang-format on
