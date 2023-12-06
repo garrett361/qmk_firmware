b39832b4fb67d720b123bcbccba9cb0dff886cb6
Garrett Goon <garrett.goon@hpe.com>
2023-12-06 09:06:18 -0500

    starting wylder keymap creation

---
+++ keyboards/handwired/wylderbuilds/trackball/3x5_3_track/keymaps/garrett361/docs_from_wylder/3x5_3_track/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x61, 0xC2, 0x67, 0x3C, 0x1C, 0x2C, 0xA7, 0x57}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 3 }
